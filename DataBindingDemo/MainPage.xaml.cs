f0c2e256dca3b4894f27d9dbc29d9bd967e93949
Wolvye <alainsoltau@googlemail.com>
2025-06-04 19:29:21 +0200

    Add project files.

---
+++ DataBindingDemo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DataBindingDemo.Models;
+
+namespace DataBindingDemo
+{
+    public partial class MainPage : ContentPage
+    {
+  
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            var person = new Person
+            { 
+            Name = "John",
+            Phone = "999",
+            Address = "X Address"
+            };
+        }
+    }
+}
